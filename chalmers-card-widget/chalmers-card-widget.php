03c8fbaf447fa40e08c7297add32f4e39f931c90
Johan Winther <johan.winther@outlook.com>
2017-11-26 19:02:43 +0100

    Moved user meta to plugin instead of theme

--- chalmers-card-widget/chalmers-card-widget.php
+++ chalmers-card-widget/chalmers-card-widget.php
@@ -100,3 +100,61 @@ class ChalmersCardWidget extends WP_Widget {
 		return $instance;
 	}
 } // Class chalmers_card_widget ends here
+
+
+/*
+* Profile field for Student Union card
+*/
+add_action( 'show_user_profile', 'user_meta_show_form_field_chalmers_card' );
+add_action( 'edit_user_profile', 'user_meta_show_form_field_chalmers_card' );
+
+function user_meta_show_form_field_chalmers_card( $user ) { ?>
+
+	<h3>Chalmers</h3>
+
+	<table class="form-table">
+		<tr>
+			<th>
+				<label for="chalmers_card">Kårkort</label>
+			</th>
+			<td>
+				<input type="number"
+				class="regular-text ltr"
+				id="chalmers-card"
+				name="chalmers-card"
+				value="<?= esc_attr(get_user_meta($user->ID, 'chalmers-card', true)); ?>"
+				title="You can find your 16 digit number on your Student Union Card."
+				pattern="\d{16}"
+				required>
+				<p class="description">
+					Skriv in hela numret för ditt kårkort. Detta måste uppdateras när du får ett nytt.
+				</p>
+			</td>
+		</tr>
+	</table>
+<?php }
+
+add_action( 'personal_options_update', 'user_meta_update_form_field_chalmers_card' );
+add_action( 'edit_user_profile_update', 'user_meta_update_form_field_chalmers_card' );
+
+/**
+* The save action.
+*
+* @param $user_id int the ID of the current user.
+*
+* @return bool Meta ID if the key didn't exist, true on successful update, false on failure.
+*/
+function user_meta_update_form_field_chalmers_card( $user_id ) {
+
+	// check that the current user have the capability to edit the $user_id
+	if (!current_user_can('edit_user', $user_id)) {
+		return false;
+	}
+
+	// create/update user meta for the $user_id
+	return update_user_meta(
+		$user_id,
+		'chalmers-card',
+		$_POST['chalmers-card']
+	);
+}
