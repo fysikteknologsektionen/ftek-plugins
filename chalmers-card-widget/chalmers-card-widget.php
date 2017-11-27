e09a2aed7808cc6af5ab2244723bd48990e584c0
Fysikteknologsektionen <karnstyret@ftek.se>
2017-11-27 18:32:58 +0100

    Better error handling

--- chalmers-card-widget/chalmers-card-widget.php
+++ chalmers-card-widget/chalmers-card-widget.php
@@ -59,22 +59,26 @@ class ChalmersCardWidget extends WP_Widget {
         $cardNumber = get_user_meta(get_current_user_id(), 'chalmers-card', true);
         if ($cardNumber != "") {
             $cardObject = chcw_get_balance($cardNumber);
-            if (!$cardObject && !isset($cardObject->error)) {
-                $title = apply_filters( 'widget_title', $instance['title'] );
+            
+            $title = apply_filters( 'widget_title', $instance['title'] );
 
-                // before and after widget arguments are defined by themes
-                echo $args['before_widget'];
-                if ( ! empty( $title ) )
+            // before and after widget arguments are defined by themes
+            echo $args['before_widget'];
+            if ( ! empty( $title ) )
                 echo $args['before_title'] . $title . $args['after_title'];
 
-                // This is where you run the code and display the output
-
+            // This is where you run the code and display the output
+            
+            // If connection timed out
+            if (!$cardObject) {
+                echo 'Could not connect to card server.';
+            } else if (isset($cardObject->error)) {
+                echo error;
+            } else {
                 echo '<span id="name">' . $cardObject->cardHolder . '</span><br>';
                 echo '<span id="balance">' . number_format_i18n($cardObject->cardBalance->value, 2). ' ' . __('SEK', 'chcw') . '</span>';
-
-
-                echo $args['after_widget'];
             }
+            echo $args['after_widget'];
         }
 	}
 
