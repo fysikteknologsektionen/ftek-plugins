cf1b21bd69e9aee9dfbd433346f91e831cadf9fb
Fysikteknologsektionen <karnstyret@ftek.se>
2017-12-18 10:36:48 +0100

    AJAX implementation

---
+++ chalmers-card-widget/chalmers-card-widget.js
@@ -0,0 +1,82 @@
+function setCookie(key, value, days) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value +';path=/'+ ';expires=' + expires.toUTCString();
+}
+
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+}
+
+function fetchCardData() {
+    $("p#error-message").text("");
+    $("div#card-message").show();
+    $("p#fetch-message").show();
+    $("div#card-input").hide();
+    var data = {
+        'action': 'card_action',
+        'card-number-encrypted': getCookie('chalmers-card')
+    };
+    $.post(ajax_object.ajax_url, data, function(cardNumber) {
+        $.ajax({
+            url: "https://ftek.se/api/card-balance/v1/" + cardNumber,
+            success: function(cardData){
+                fillCardData(cardData);
+            },
+            timeout: 3000,
+            error: function(e) {
+                resetCard();
+                $("div#card-message").show();
+                $("p#error-message").text(e.responseJSON.error);
+                $("p#error-message").show();
+            }
+        });
+    });
+}
+
+function fillCardData(cardData) {
+    $("div#card-message").hide();
+    $("#card-holder").text(cardData.cardHolder);
+    $("#card-balance").text(cardData.cardBalance.value + " " + ajax_object.currency + " ");
+    $("div#card-info").show();
+}
+
+function resetCard() {
+    setCookie('chalmers-card', "", -30 * 6);
+    $("p#error-message").text("");
+    $("div#card-info").hide();
+    $("div#card-message").hide();
+    $("p#fetch-message").hide();
+    $("div#card-input").show();
+}
+
+jQuery(document).ready(function($){
+    if (getCookie('chalmers-card') !== null) {
+        fetchCardData();
+    }
+
+    $("button#get-balance").click(function() {
+        var cardNumber = $("input#card-number").val().replace(/\s/g, '');
+        $("input#card-number").val(cardNumber);
+        if (/^\d{16}/.test(cardNumber)) {
+            var data = {
+                'action': 'card_action',
+                'card-number': cardNumber
+            };
+            $.post(ajax_object.ajax_url, data, function(response) {
+                setCookie('chalmers-card', response, 30 * 6);
+                fetchCardData();
+            });
+        } else {
+            resetCard();
+            $("div#card-message").show();
+            $("p#error-message").text(ajax_object.wrong_format);
+            $("p#error-message").show();
+        }
+    });
+
+    $("button#remove-card").click(function() {
+        resetCard();
+    });
+});
--- chalmers-card-widget/chalmers-card-widget.php
+++ chalmers-card-widget/chalmers-card-widget.php
@@ -13,6 +13,11 @@ For fetching card account balance. Uses the api at https://ftek.se/api/card-bala
 
 add_action( 'init', 'init_chcw' );
 function init_chcw() {
+
+    if ( !class_exists( 'Defuse\Crypto\Crypto' ) ) {
+        require_once(__DIR__ . '/../ftek-user-fields/vendor/autoload.php');
+    }
+
     // Load translations
     load_plugin_textdomain('chcw', false, basename( dirname( __FILE__ ) ) . '/languages' );
 }
@@ -36,6 +41,23 @@ function chalmers_card_load_widget() {
 }
 add_action( 'widgets_init', 'chalmers_card_load_widget' );
 
+// PHP encrypt and decrypt handler function
+function card_action() {
+    $key = Defuse\Crypto\Key::loadFromAsciiSafeString( CHALMERS_ENCRYPT_KEY );
+    if (isset($_POST['card-number'])) {
+        $cardNumber = $_POST['card-number'];
+        $cardNumberEncrypted = Defuse\Crypto\Crypto::encrypt($cardNumber, $key );
+        echo $cardNumberEncrypted;
+    } else if (isset($_POST['card-number-encrypted'])) {
+        $cardNumberEncrypted = $_POST['card-number-encrypted'];
+        $cardNumber = Defuse\Crypto\Crypto::decrypt($cardNumberEncrypted, $key );
+        echo $cardNumber;
+    }
+    wp_die();
+}
+add_action( 'wp_ajax_card_action', 'card_action' );
+add_action( 'wp_ajax_nopriv_card_action', 'card_action' );
+
 // Creating the widget
 class ChalmersCardWidget extends WP_Widget {
 
@@ -54,32 +76,39 @@ class ChalmersCardWidget extends WP_Widget {
     }
 
     // Creating widget front-end
-
     public function widget( $args, $instance ) {
-        $key = Defuse\Crypto\Key::loadFromAsciiSafeString( CHALMERS_ENCRYPT_KEY );
-        $cardNumber = "";
-        $cardNumberEncrypted = get_user_meta(get_current_user_id(), 'chalmers-card', true);
-        if ($cardNumberEncrypted != "")
-            $cardNumber = Defuse\Crypto\Crypto::decrypt($cardNumberEncrypted, $key );
-        
-        if ($cardNumber != "") {
-            $cardObject = chcw_get_balance($cardNumber);
-            
-            // If connection timed out
-            if ($cardObject && !isset($cardObject->error)) {
-            
-                // before and after widget arguments are defined by themes
-                echo $args['before_widget'];
-                echo $args['before_title'] . __( 'Card Balance', 'chcw' ) . $args['after_title'];
 
-                // This is where you run the code and display the output
-                echo '<p id="name">' . $cardObject->cardHolder . '</p>';
-                echo '<p id="balance">' . number_format_i18n($cardObject->cardBalance->value, 2). ' ' . __('SEK', 'chcw') . '</p>';
-                echo '<p id="card-link"><a href="https://kortladdning3.chalmerskonferens.se" target="_blank">' . __('Charge card','chcw') . '</a></p>';
-                
-                echo $args['after_widget'];
-            }
-        }
+        wp_enqueue_script('card-script', plugin_dir_url(__FILE__) . '/chalmers-card-widget.js', array('jquery'));
+        wp_localize_script( 'card-script', 'ajax_object', array(
+            'ajax_url' => admin_url( 'admin-ajax.php' ),
+            'currency' => __('SEK', 'chcw'),
+            'wrong_format' => __('Please input 16 digits.', 'chcw')
+        ));
+
+        // before and after widget arguments are defined by themes
+        echo $args['before_widget'];
+        echo $args['before_title'] . __( 'Card Balance', 'chcw' ) . $args['after_title'];
+
+        echo '<div id="card-message" style="display:none">';
+        echo '<p id="fetch-message">' . __('Fetching balance...', 'chcw') . '</p>';
+        echo '<p id="error-message" style="display:none">' . __('Could not fetch balance.', 'chcw') . '</p>';
+        echo '</div>';
+
+        echo '<div id="card-info" style="display:none">';
+        echo '<p id="card-holder"></p>';
+        echo '<p><span id="card-balance"></span><a href="https://kortladdning3.chalmerskonferens.se" target="_blank">'
+        . __('Charge card','chcw') . '</a></p>';
+        echo '<p><button id="remove-card" style="margin-top:0.5em">' . __('Remove card', 'chcw') . '</button></p>';
+        echo '</div>';
+
+        echo '<div id="card-input">';
+        echo '<label for="card-number">' . __('Card number','chcw') . '</label>';
+        echo '<input type="text" id="card-number" style="margin: 0.5em 0em;padding: 0.3em 0.15em;" placeholder="#### #### #### ####">';
+        echo '<button id="get-balance">' . __('Get balance', 'chcw') . '</button>';
+        echo '</div>';
+
+        echo $args['after_widget'];
+
     }
 
     // Widget Backend
