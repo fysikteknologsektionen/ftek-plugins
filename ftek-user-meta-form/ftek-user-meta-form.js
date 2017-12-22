6e4f894a4e7cc10c27cff764c8ffd83cbad73b35
Johan Winther <johan.winther@outlook.com>
2017-12-22 19:23:54 +0100

    Update ftek-user-meta-form.js

--- ftek-user-meta-form/ftek-user-meta-form.js
+++ ftek-user-meta-form/ftek-user-meta-form.js
@@ -3,24 +3,27 @@ jQuery(document).ready(function($){
     $("form#meta-form").submit(function(e) {
 
         e.preventDefault();
-        var regexPersonalNumber = /[0-9]{2}((0[0-9])|(10|11|12))(([0-2][0-9])|(3[0-1]))-[0-9]{4}/;
-        var personalNumber = $("#personal-number").val().replace(" ","");
-        if (regexPersonalNumber.test(personalNumber)) {
-            personalNumber = personalNumber.match(regexPersonalNumber)[0];
-            $("#personal-number").val(personalNumber);
-            $.post( ftek_user_meta_obj.ajaxurl, {
-                action : 'ftek_update_meta',
-                post : $("form#meta-form").serialize()
-            },
-            function(response) {
-                if (response === "Updated") {
-                    $("#personal-number-message").text("");
-                    $("#form-message").text(" Sparat.");
-                }
-            });
-        } else {
-            $("#personal-number-message").text(" YYMMDD-XXXX.");
+        if ($("#personal-number").length != 0) {
+            var regexPersonalNumber = /[0-9]{2}((0[0-9])|(10|11|12))(([0-2][0-9])|(3[0-1]))-[0-9]{4}/;
+            var personalNumber = $("#personal-number").val().replace(" ","");
+            if (regexPersonalNumber.test(personalNumber)) {
+                personalNumber = personalNumber.match(regexPersonalNumber)[0];
+                $("#personal-number").val(personalNumber);
+            } else {
+                $("#personal-number-message").text(" YYMMDD-XXXX.");
+                return false;
+            }
         }
+        $.post( ftek_user_meta_obj.ajaxurl, {
+            action : 'ftek_update_meta',
+            post : $("form#meta-form").serialize()
+        },
+        function(response) {
+            if (response === "Updated") {
+                $("#personal-number-message").text("");
+                $("#form-message").text(" Sparat.");
+            }
+        });
 
         return false; // Prevent form from submitting
     });
