49a6ee039b7ca4b359058911dbd02dee68e95cf4
Eric Carlsson <ericca@student.chalmers.se>
2018-01-21 12:47:08 +0100

    Changed mail-to adress to be WordPress admin mail.
    Changed hyperlink url to be dynamic so long as default support pagei s
    used. (Could be made entirely dynamic by getting appropiate redirect url
    from wpdb, which would require a second query)
    
    Fixed issue with wpdb prefix not being properly parsed in query string.
    
    Slight tweaks.

--- wpsp-mail-notification/wpsp-mail-notification.php
+++ wpsp-mail-notification/wpsp-mail-notification.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: WP Support Plus: Mail Notification
-Description: Sends an e-mail with some details to a predefined adress when a new ticket is created in WP Support Plus.
+Description: Sends an e-mail to the site admin when a new ticket is created in WP Support Plus.
 Author: Eric Carlsson
 */
 
@@ -13,19 +13,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_action( 'wpsp_after_create_ticket', 'wpsp_mail' );
 function wpsp_mail($ticket_id) {
   global $wpdb;
-  $sql = $wpdb->prepare('SELECT subject, guest_name, guest_email, create_time FROM {$wpdb->prefix}wpsp_ticket WHERE id=%d', $ticket_id);
+  $sql = $wpdb->prepare("SELECT subject, guest_name, guest_email, create_time FROM {$wpdb->prefix}wpsp_ticket WHERE id=%d", $ticket_id);
   $result = $wpdb->get_row($sql);
   if (!is_null($result)) {
-    $mail_to = 'spidera@ftek.se';
+    $mail_to = get_bloginfo('admin_email');
     $mail_subject = '[Ticket #' . $ticket_id . '] WP Support Plus notification';
-    $mail_message = '<html><font size=4>A new ticket has been created</font><br /><b>Author:</b> ' . $result->guest_name
+		$support_url = get_site_url(null, '/support/?page=tickets&section=ticket-list&action=open-ticket&id=' . $ticket_id);
+    $mail_message = '<html><font size=4>A new ticket has been created in WP Support Plus</font><br /><b>Author:</b> ' . $result->guest_name
                     . '<br \><b>Author email:</b> ' . $result->guest_email
                     . '<br \><b>Subject:</b> ' . $result->subject
                     . '<br \><b>Date:</b> ' . $result->create_time
-                    . '<br \><a href="https://ftek.se/support/?page=tickets&section=ticket-list&action=open-ticket&id='
-                    . $ticket_id . '"><b>View ticket by clicking here</b></a>' . '</html>';
+                    . '<br \><a href="' . $support_url . '"><b>View ticket by clicking here</b></a></html>';
     $mail_headers = array('Content-Type: text/html; charset=UTF-8');
-
     wp_mail($mail_to, $mail_subject, $mail_message, $mail_headers);
   }
 }
