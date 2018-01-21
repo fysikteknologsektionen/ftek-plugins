fdee0ccce38a3325d898a7ef71f29044a94fbf0c
Johan Winther <johan.winther@outlook.com>
2018-01-21 15:20:40 +0100

    More straightforward text in email

--- wpsp-mail-notification/wpsp-mail-notification.php
+++ wpsp-mail-notification/wpsp-mail-notification.php
@@ -17,13 +17,13 @@ function wpsp_mail($ticket_id) {
   $result = $wpdb->get_row($sql);
   if (!is_null($result)) {
     $mail_to = get_bloginfo('admin_email');
-    $mail_subject = '[Ticket #' . $ticket_id . '] WP Support Plus notification';
-		$support_url = get_site_url(null, '/support/?page=tickets&section=ticket-list&action=open-ticket&id=' . $ticket_id);
+    $mail_subject = 'Ftek Support - Ticket #' . $ticket_id;
+	  $support_url = get_site_url(null, '/support/?page=tickets&section=ticket-list&action=open-ticket&id=' . $ticket_id);
     $mail_message = '<html><font size=4>A new ticket has been created in WP Support Plus</font><br /><b>Author:</b> ' . $result->guest_name
                     . '<br \><b>Author email:</b> ' . $result->guest_email
                     . '<br \><b>Subject:</b> ' . $result->subject
                     . '<br \><b>Date:</b> ' . $result->create_time
-                    . '<br \><a href="' . $support_url . '"><b>View ticket by clicking here</b></a><br \><br \></html>';
+                    . '<br \><a href="' . $support_url . '"><b>View ticket</b></a><br \><br \></html>';
     $mail_headers = array('Content-Type: text/html; charset=UTF-8');
     wp_mail($mail_to, $mail_subject, $mail_message, $mail_headers);
   }
