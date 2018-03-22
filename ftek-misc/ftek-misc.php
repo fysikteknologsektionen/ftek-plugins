a7745bef67879b199243090073b395ebc00865b6
Johan Winther <johan.winther@outlook.com>
2018-03-22 16:13:46 +0100

    Update ftek-misc.php

--- ftek-misc/ftek-misc.php
+++ ftek-misc/ftek-misc.php
@@ -11,5 +11,8 @@ if ( !function_exists( 'wp_password_change_notification' ) ) {
     function wp_password_change_notification() {}
 }
 
+/* Auto update plugins */
+add_filter( 'auto_update_plugin', '__return_true' );
+
 /* Stop Adding Functions Below this Line */
 ?>
