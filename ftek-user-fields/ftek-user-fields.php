3519f451a2d24ed32a3cdf76a16843f3cf675f15
Johan Winther <johan.winther@outlook.com>
2018-04-26 20:03:52 +0200

    Class profile edit fields

--- ftek-user-fields/ftek-user-fields.php
+++ ftek-user-fields/ftek-user-fields.php
@@ -7,14 +7,9 @@ Text Domain: ftek_uf
 Domain Path: /languages
 */
 
-
-/*
-* Load encryption library
-*/
-
 add_action( 'init', 'init_ftek_uf' );
 function init_ftek_uf() {
-    // Load translations
+    // Load translations (none at the moment)
     load_plugin_textdomain('ftek_uf', false, basename( dirname( __FILE__ ) ) . '/languages' );
 
     if ( current_user_can('edit_users') ) {
@@ -26,7 +21,7 @@ function init_ftek_uf() {
 }
 
 /**
-* Adds a new column to the users table to show the personal ID number
+* Adds a new column to the users table to show the class
 *
 * @param array $column_headers
 * @return array column headers
@@ -56,4 +51,65 @@ function ftek_uf_manage_users_custom_column( $output, $column_name, $user_id ) {
         }
         return $output;
     }
+}
+
+/*
+* Class field on edit profile page
+*/
+add_action( 'show_user_profile', 'user_meta_show_form_field_class' );
+add_action( 'edit_user_profile', 'user_meta_show_form_field_class' ); // Show class for administrators
+function user_meta_show_form_field_class( $user ) {
+    
+    $class = get_user_meta($user->ID, 'class' , true);
+    if (!empty($class)) {
+        $program = strrev(substr(strrev($class),2));
+        $year = substr($class,2);
+    } else {
+        $program = "";
+        $year = "";
+    }
+    ?>
+    <script>
+         jQuery(document).ready(function ($) {
+             $("tr.user-description-wrap").before('\
+                <tr class="user-class">\
+                    <th><label for="year">Årskurs</label></th>\
+                    <td>\
+                        <select class="program" name="program">\
+                            <option <?= echo($program ? "" : "selected") ?> disabled hidden value="">Program</option>\
+                            <option <?= echo($program === "f" ? "selected" : "") ?> value="f">F</option>\
+                            <option <?= echo($program === "tm" ? "selected" : "") ?> value="tm">TM</option>\
+                        </select>\
+                        <input name="year" type="number" pattern="[0-9]{2}" placeholder="YY" value="<?= echo $year ?>">\
+                        <p class="description">Här kan du välja klass för att skräddarsy hemsidan och den mejl du får. Ytterligare mejlinställningar går att hitta via länken längst ner i något av våra utskick.</p>\
+                    </td>\
+                </tr>\
+             ');
+         });
+    </script>
+<?php 
+}
+
+
+add_action( 'personal_options_update', 'user_meta_update_form_field_class' );
+add_action( 'edit_user_profile_update', 'user_meta_update_form_field_class' );
+/**
+* The save action.
+*
+* @param $user_id int the ID of the current user.
+*
+* @return bool Meta ID if the key didn't exist, true on successful update, false on failure.
+*/
+function user_meta_update_form_field_class( $user_id ) {
+    $class = $_POST['program'] . substr($_POST['year'],-2); //
+    
+    // check that the current user have the capability to edit the $user_id
+    if (!current_user_can('edit_user', $user_id)) {
+        return false;
+    }
+    
+    if (!preg_match('/(f|tm)[0-9]{2}/', $class)) {
+        $class = "";
+    }
+    return update_user_meta($user_id, 'class', $class);
 }
